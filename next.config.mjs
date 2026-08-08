f8abfc88542ac5565657be66d4e43bca3b294732
Seniorcaptain <36276653+Seniorcaptain@users.noreply.github.com>
2026-08-08 14:48:14 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  images: {
+    unoptimized: false,
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 365,
+    dangerouslyAllowSVG: false,
+    contentSecurityPolicy: "default-src 'none'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fivestardrivingschools.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+  headers: async () => [
+    {
+      source: "/images/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=31536000, immutable",
+        },
+      ],
+    },
+  ],
+}
+
+export default nextConfig
